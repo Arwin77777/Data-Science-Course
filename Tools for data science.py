80949a24d1291928e8d3ea42a418623866a6e1ea
Arwin Sekar <100914887+Arwin77777@users.noreply.github.com>
2023-08-27 13:18:53 +0530

    Add files via upload

---
+++ Tools for data science.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Notebook
+
+# Introduction
+
+# Python
+# R
+# SQL
+# Java
+# Julia
+# Scala
+# C/C++
+# JavaScript
+
+# TensorFlow
+# NumPy
+# SciPy
+# Pandas
+# Matplotlib
+# Keras
+# SciKit-Learn
+# PyTorch
+# Scrapy
+# BeautifulSoup
+# LightGBM
+# ELI5
+# Theano
+# NuPIC
+# Ramp
+# Pipenv
+
+# SAS
+# Apachespark
+# BigML
+
+# In[2]:
+
+
+# Arithmetic operations
+code = compile("5 + 4", "<string>", "eval")
+eval(code)
+
+
+# In[3]:
+
+
+code1 = compile("(5 + 7) * 2", "<string>", "eval")
+eval(code1)
+
+
+# In[4]:
+
+
+import math
+code2 = compile("4 / 3 * math.pi * math.pow(25, 3)", "<string>", "eval")
+eval(code2)
+
+
+# In[5]:
+
+
+(3*4)+5
+
+
+# In[6]:
+
+
+days = 0
+hours = 0
+mins = 0
+
+time = 200
+#days = time / 1440
+leftover_minutes = time % 1440
+hours = leftover_minutes / 60
+#mins = time - (days*1440) - (hours*60)
+print(str(days) + " days, " + str(hours) + " hours, " + str(mins) +  " mins. ")
+
+
+# Objectives
+# 
+# Popular languages for data science 
+# Basic arithmetic operations
+# Working on markdowns
+
+# Author : Arwin 
+
+# In[ ]:
+
+
+
+
